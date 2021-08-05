eb24feecc88d0b93ac3a093c5694d4494ae5de09
Sumit70421 <sp39605@gmail.com>
2021-08-05 14:52:40 +0530

    card title now appears as we give input

--- todo.js
+++ todo.js
@@ -1,5 +1,28 @@
-var add= document.querySelector("span#addlist")
+// Index Page 
 
-add.addEventListener('click', ()=>{
-    alert("lmao it works fine")
+var masterList= [];
+var addNewList= document.querySelector("span#addlist")
+// adding new list 
+addNewList.addEventListener('click', ()=>{
+    var blur = document.getElementById("main-page");
+    blur.classList.add('blurClass');
+    var popUp = document.getElementById("popUp")
+    popUp.style.display="flex";
 })
+function makeCard(name){
+
+}
+
+function addMasterItem(){
+    var listName= document.getElementById("listName").value;
+    masterList.push(listName);
+    var cardTemp = document.getElementById("title")
+    cardTemp.innerHTML=masterList[0];   
+}
+
+
+
+
+
+
+
